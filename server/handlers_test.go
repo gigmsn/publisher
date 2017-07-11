45c0168eb6162309df7426db14b184502d522a3c
Alessandro Resta <alessandro.resta@gmail.com>
2017-07-11 10:24:34 +0100

    Improve test for wsHandler

--- server/handlers_test.go
+++ server/handlers_test.go
@@ -28,6 +28,8 @@ func TestIndexHandler(t *testing.T) {
 
 // test websocket connection on '/ws' endpoint
 func TestWsHandler(t *testing.T) {
+	expectedResponse := []byte("acknowledged")
+
 	server := httptest.NewServer(serverEngine())
 	defer server.Close()
 
@@ -38,8 +40,22 @@ func TestWsHandler(t *testing.T) {
 
 	URL.Scheme = "ws"
 
-	_, _, err = websocket.DefaultDialer.Dial(URL.String(), nil)
+	conn, _, err := websocket.DefaultDialer.Dial(URL.String(), nil)
 	if err != nil {
 		t.Errorf("expected to create websocket connection with success instead: %s", err)
+	}
+
+	if err := conn.WriteMessage(websocket.TextMessage, []byte("test")); err != nil {
+		t.Errorf("could not write message: %s", err)
+	}
+
+	_, observed, err := conn.ReadMessage()
+	if err != nil {
+		t.Errorf("cannot read message: %v", err)
+	}
+
+	if string(observed) != string(expectedResponse) {
+		t.Errorf("expected server response: %s, observed: %s",
+			string(expectedResponse), string(observed))
 	}
 }
