d70109fad20201663f549b9ef0feea6f1c816ab7
Alessandro Resta <alessandro.resta@gmail.com>
2017-07-11 01:52:45 +0100

    Move message broker client logic to messagebroker repository

--- server/handlers.go
+++ server/handlers.go
@@ -3,6 +3,7 @@ package server
 import (
 	"net/http"
 
+	"github.com/gigmsn/messagebroker"
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
 	log "github.com/sirupsen/logrus"
@@ -21,17 +22,17 @@ func wsHandler(c *gin.Context) {
 }
 
 func ws(conn *websocket.Conn) {
-	broker, err := new("amqp://guest:guest@broker:5672", "gigmsn")
+	broker, err := messagebroker.New("amqp://guest:guest@broker:5672", "gigmsn")
 	if err != nil {
 		log.Fatalf("could not create broker: %s", err)
 	}
-	defer broker.close()
+	defer broker.Close()
 	log.Infoln("broker connection established")
 
 	msgCh := make(chan []byte, 100)
 	doneCh := make(chan bool)
 
-	go broker.publish(msgCh, doneCh)
+	go broker.Publish(msgCh, doneCh)
 
 	for {
 		_, msg, err := conn.ReadMessage()
@@ -43,7 +44,7 @@ func ws(conn *websocket.Conn) {
 			conn.Close()
 
 			// Close amqp channel
-			broker.channel.Close()
+			broker.Channel.Close()
 
 			// notify publisher to close msg channel
 			<-doneCh
